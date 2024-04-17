a6c1e2a24481b47b7180cbaa5ea13b147a9d4bbf
MiskuZero <739740010@qq.com>
2024-04-18 03:33:33 +0800

    实现人物跳跃和重力加速度模拟！

--- src/RibbonDemo.java
+++ src/RibbonDemo.java
@@ -23,63 +23,80 @@ public class RibbonDemo extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.WHITE);
         g.drawImage(bg, xbg, -height, this);
-        g.drawRect(x, y, 50, 50);
+        g.setColor(Color.YELLOW);
+        g.drawRect(x, y, 20, 35);
     }
 
     public RibbonDemo() {
         height = 600;
         width = 800;
         x = 100;
-        y = height - 80;
+        y = height - 68;
         bg = getToolkit().createImage("E:\\我的收藏\\美图\\wallhaven-8ogjek.jpg");
         playAnimation();
-    }
-
-    private static double secondsAt(double start) {
-        return (System.currentTimeMillis() - start) / 1000;
     }
 
     private void playAnimation() {
         new Thread(() -> {
             final int lBound = 50, rBound = width - 100;//窗口边界
             final int blBound = width - 1600, brBound = 0;//卷轴（背景图）边界
+            final int dBound = y;//下边界
             final long delay = 1;//延迟（与帧率成反比）
-            final double a = 2000;//加速度常量（pixel/s^2）
-            final double maxV = 600;//最大速率（pixel/s）
+            final double g = 10000;//重力加速度（pixel/s^2）
+            final double a = 2000;//右移动加速度（pixel/s^2）
+            final double vJump = -2000;//跳跃速度（pixel/s）
+            final double vxMax = 500;//最大速率（pixel/s）
             final double deltaT = (double) delay / 1000;
             final double factor = 0.2;//摩擦系数
             double xl = x;//左位移（pixel）
             double xr = x;//右位移（pixel）
             double xm = x;//不位移（pixel）
-            double v = 0;//速度（pixel/s）
+            double y = dBound;//高度位移（pixel）
+            double vx = 0;//x速度（pixel/s）
+            double vy = -vJump;//y速度（pixel/s）
             while (true) {
+                //确定横向参数
                 if (lMove && !rMove) {//仅按A键
-                    v -= a * deltaT;
-                    if (v > maxV)
-                        v = maxV;
-                    else if (v < -maxV)
-                        v = -maxV;
-                    x = (int) (xr = xm = xl += v * deltaT);
+                    if (this.y == dBound) {//在地上
+                        vx -= a * deltaT;
+                        if (vx > vxMax)
+                            vx = vxMax;
+                        else if (vx < -vxMax)
+                            vx = -vxMax;
+                    }
+                    x = (int) (xr = xm = xl += vx * deltaT);
                 } else if (rMove && !lMove) {//仅按D键
-                    v += a * deltaT;
-                    if (v > maxV)
-                        v = maxV;
-                    else if (v < -maxV)
-                        v = -maxV;
-                    x = (int) (xl = xm = xr += v * deltaT);
+                    if (this.y == dBound) {//在地上
+                        vx += a * deltaT;
+                        if (vx > vxMax)
+                            vx = vxMax;
+                        else if (vx < -vxMax)
+                            vx = -vxMax;
+                    }
+                    x = (int) (xl = xm = xr += vx * deltaT);
                 } else {
-                    if (v > factor)
-                        v -= factor;
-                    else if (v < -factor)
-                        v += factor;
+                    if (vx > factor)
+                        vx -= factor;
+                    else if (vx < -factor)
+                        vx += factor;
                     else
-                        v = 0;
-                    x = (int) (xl = xr = xm += v * deltaT);
+                        vx = 0;
+                    x = (int) (xl = xr = xm += vx * deltaT);
                 }
-//                if (jump) ;
-                System.out.print("\rx: " + x + ",v: " + v);
+                //确定纵向参数
+                if (this.y == dBound) {//在地上
+                    if (jump)
+                        vy = vJump;
+                } else
+                    vy += g * deltaT;
+                y += vy * deltaT;
+                if (y > dBound)
+                    y = dBound;
+                this.y = (int) y;
+                //调试
+                System.out.print("\rx: " + x + ", y: " + this.y + ", vx: " + vx + ", vy: " + vy);
+                //渲染
                 repaint();
                 try {
                     Thread.sleep(delay);
